3d59fc41be9c352e8716eb7240fb24b41d0622ce
Rik <ricardomartins145@gmail.com>
2025-10-03 13:54:39 +0100

    Login completed, design improvements still needed, rest of app unchanged

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import pg from "pg";
+import result from "pg/lib/query";
 
 const app = express();
 const port = 3000;
 let value = "";
-const bookCoverAPI = `https://covers.openlibrary.org/b/isbn/${value}-M.jpg`;
+// const bookCoverAPI = `https://covers.openlibrary.org/b/isbn/${value}-M.jpg`;
 
 const db = new pg.Pool({
     user: "postgres",
@@ -19,33 +20,42 @@ const db = new pg.Pool({
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+async function login() {
+    const resultData = await db.query("SELECT * FROM users");
+    const loginData = resultData.rows;
+
+    return loginData;
+}
+
 app.get("/", async (req, res) => {
-    const result = await db.query("SELECT * FROM book");
-    const bookNotesData = [];
+    //do i really need to pass info here?
+    //isnt it just to render login page and then when button clicked check if every thing matches=
+    const loginData = login();
+    res.render("login.ejs", {loginData: loginData});
+});
 
-    for (const book of result.rows) {
-        let coverURL = '/images/bookPlaceholder.jpeg';
+app.post("/login", async (req, res) => {
+    try {
+        const username = req.body.username.toLowerCase();
+        const password = req.body.password.toLowerCase();
 
-        try {
-            // const resultAPI = await axios.get(`https://covers.openlibrary.org/b/ISBN/${book.isbn}-M.jpg?default=false`);
-            //
-            // if (resultAPI) {
-            //     coverURL = `https://covers.openlibrary.org/b/ISBN/${resultAPI}`;
-            // }
-        } catch (error) {
-            console.error(error);
-            console.log(`Error fetching book cover: using Placeholder.`);
+        const resultData = await db.query("SELECT * FROM users");
+
+        for (const user of resultData.rows) {
+            if (user.username.toLowerCase() === username && user.password.toLowerCase() === password) {
+                const result = await db.query("SELECT * FROM book WHERE userid = $1", [user.id]);
+                const bookNotesData = result.rows;
+
+                res.render("index.ejs", {bookNotesData: bookNotesData});
+            } else {
+                res.render("login.ejs", {errorMessage: "Username or password is incorrect"});
+            }
         }
 
-        bookNotesData.push({
-            ...book,
-            coverURL
-        });
+    } catch (err) {
+        console.log(err);
     }
-
-
-    res.render("index.ejs", {bookNotesData: bookNotesData});
-});
+})
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
